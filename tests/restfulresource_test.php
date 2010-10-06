af1ae123abbe377841669b5dc33c086ffc2a4669
Brian Brennan <brianloveswords@gmail.com>
2010-10-06 16:42:07 -0400

    Redid the menu generator.

--- tests/restfulresource_test.php
+++ tests/restfulresource_test.php
@@ -1,15 +1,16 @@
 <?php
 require_once "simpletest/autorun.php";
 require_once "../restfulresource_class.inc";
+require_once "../restfulresource.module";
 
 class RestfulResourceTest extends UnitTestCase {
   function testShouldCreateNewRestfulResource() {
-    $rr = new RestfulResource('test', 'tests', 'test');
+    $rr = new RestfulResource('test', 'tests', 'testmodule');
     $this->assertIsA($rr, RestfulResource);
   }
   
   function testShouldHaveMethodSubClasses() {
-    $rr = new RestfulResource('test', 'tests',  'test');
+    $rr = new RestfulResource('test', 'tests',  'testmodule');
     $this->assertIsA($rr->post, HTTPPost);
     $this->assertIsA($rr->get, HTTPGet);
     $this->assertIsA($rr->put, HTTPPut);
@@ -17,7 +18,7 @@ class RestfulResourceTest extends UnitTestCase {
   }
   
   function testShouldHaveMethodDefaultActions() {
-    $rr = new RestfulResource('test', 'tests', 'test');
+    $rr = new RestfulResource('test', 'tests', 'testmodule');
     $this->assertNotNull($rr->post->actions['create']);
     $this->assertNotNull($rr->get->actions['read']);
     $this->assertNotNull($rr->put->actions['update']);
@@ -25,18 +26,21 @@ class RestfulResourceTest extends UnitTestCase {
   }
   
   function testShouldRemoveDefaultAction() {
-    $rr = new RestfulResource('test', 'tests', 'test');
+    $rr = new RestfulResource('test', 'tests', 'testmodule');
     $rr->post->removeAction('create');
     $this->assertNull($rr->post->actions['create']);
   }
 
   function testShouldGenerateDrupalMenu() {
-    $rr = new RestfulResource('test', 'tests', 'test');
-    $menu = $rr->generateMenu('/api/1.1/');
+    $rr = new RestfulResource('test', 'tests', 'testmodule', '/api/1.1/');
+    $rr2 = new RestfulResource('hand', 'hands', 'handmodule', '/api/');
+    $menu = RestfulResource::generateMenu();
     
     $this->assertNotNull($menu['api/1.1/test.json']);
     $this->assertNotNull($menu['api/1.1/test.txt']);
     $this->assertNotNull($menu['api/1.1/tests/%']);
-    $this->assertNotNull($menu['api/1.1/tests/%/%']);
+    $this->assertNotNull($menu['api/hand.json']);
+    $this->assertNotNull($menu['api/hand.txt']);
+    $this->assertNotNull($menu['api/hands/%']);
   }
 }
