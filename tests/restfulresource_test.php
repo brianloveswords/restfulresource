4d82f9d3697a5add73241679854cb17a5478a709
Brian Brennan <brianloveswords@gmail.com>
2010-10-06 16:14:17 -0400

    Menu generator.

--- tests/restfulresource_test.php
+++ tests/restfulresource_test.php
@@ -4,12 +4,12 @@ require_once "../restfulresource_class.inc";
 
 class RestfulResourceTest extends UnitTestCase {
   function testShouldCreateNewRestfulResource() {
-    $rr = new RestfulResource('test', 'test');
+    $rr = new RestfulResource('test', 'tests', 'test');
     $this->assertIsA($rr, RestfulResource);
   }
   
   function testShouldHaveMethodSubClasses() {
-    $rr = new RestfulResource('test', 'test');
+    $rr = new RestfulResource('test', 'tests',  'test');
     $this->assertIsA($rr->post, HTTPPost);
     $this->assertIsA($rr->get, HTTPGet);
     $this->assertIsA($rr->put, HTTPPut);
@@ -17,7 +17,7 @@ class RestfulResourceTest extends UnitTestCase {
   }
   
   function testShouldHaveMethodDefaultActions() {
-    $rr = new RestfulResource('test', 'test');
+    $rr = new RestfulResource('test', 'tests', 'test');
     $this->assertNotNull($rr->post->actions['create']);
     $this->assertNotNull($rr->get->actions['read']);
     $this->assertNotNull($rr->put->actions['update']);
@@ -25,8 +25,18 @@ class RestfulResourceTest extends UnitTestCase {
   }
   
   function testShouldRemoveDefaultAction() {
-    $rr = new RestfulResource('test', 'test');
+    $rr = new RestfulResource('test', 'tests', 'test');
     $rr->post->removeAction('create');
     $this->assertNull($rr->post->actions['create']);
+  }
+
+  function testShouldGenerateDrupalMenu() {
+    $rr = new RestfulResource('test', 'tests', 'test');
+    $menu = $rr->generateMenu('/api/1.1/');
+    
+    $this->assertNotNull($menu['api/1.1/test.json']);
+    $this->assertNotNull($menu['api/1.1/test.txt']);
+    $this->assertNotNull($menu['api/1.1/tests/%']);
+    $this->assertNotNull($menu['api/1.1/tests/%/%']);
   }
 }
