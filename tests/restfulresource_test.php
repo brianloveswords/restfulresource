326c2ddc424eb43a3982691ec3189ebd76548a0b
Brian Brennan <brianloveswords@gmail.com>
2010-10-06 15:17:33 -0400

    Implemented HTTPMethod class and children classes.

--- tests/restfulresource_test.php
+++ tests/restfulresource_test.php
@@ -3,4 +3,24 @@ require_once "simpletest/autorun.php";
 require_once "../restfulresource_class.inc";
 
 class RestfulResourceTest extends UnitTestCase {
+  function testShouldCreateNewRestfulResource() {
+    $rr = new RestfulResource('test', 'test');
+    $this->assertIsA($rr, RestfulResource);
+  }
+  
+  function testShouldHaveMethodSubClasses() {
+    $rr = new RestfulResource('test', 'test');
+    $this->assertIsA($rr->post, HTTPPost);
+    $this->assertIsA($rr->get, HTTPGet);
+    $this->assertIsA($rr->put, HTTPPut);
+    $this->assertIsA($rr->delete, HTTPDelete);
+  }
+  
+  function testShouldHaveMethodDefaultActions() {
+    $rr = new RestfulResource('test', 'test');
+    $this->assertNotNull($rr->post->actions['create']);
+    $this->assertNotNull($rr->get->actions['read']);
+    $this->assertNotNull($rr->put->actions['update']);
+    $this->assertNotNull($rr->delete->actions['delete']);
+  }
 }
